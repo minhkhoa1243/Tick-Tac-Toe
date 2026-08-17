88e6980d6c7090fff473a6119153b1e6e5aa1577
NoobieK <khoa.pham2550104@hcmut.edu.vn>
2026-08-17 15:59:51 +0700

    Testing

--- python_code/TTT_gameplay.py
+++ python_code/TTT_gameplay.py
@@ -1,15 +1,58 @@
-# ---
-# jupyter:
-#   jupytext:
-#     cell_metadata_filter: -all
-#     custom_cell_magics: kql
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
-#   kernelspec:
-#     display_name: ML
-#     language: python
-#     name: python3
-# ---
+# %%
+import numpy as np
+from TTT_base import TickTacToe
+
+
+
+# %%
+class TTT_gameplay(TickTacToe):
+    
+    
+    def __init__(self):
+        mark1 = input("Choose your mark: (Player1)")
+        mark2 = input("Choose your mark: (Player2)")
+        super().__init__(mark1, mark2)
+        
+        self.first = self.chooseFirst()
+        
+        
+    def chooseFirst(self):
+        first = np.random.choice([-1,1])
+        return first
+            
+        
+    
+    def gameplay(self):
+        current_player = self.first
+        print(f"First player to move is : {self.number_symbol_encode[current_player]}")
+        
+        while not self.win:
+            self.printGrid()
+            print(f"Player {self.number_symbol_encode[current_player]} turn")
+            row ,col = 0, 0
+             
+            while row < 1 or row > 3 or col < 1 or col > 3:
+                move = input("Please enter again the valid grid")
+                row, col = map(int, move.split()) # To convert string to int
+            row -= 1
+            col -= 1
+            self.mark(row, col, current_player)
+            self.checkWinner()
+            
+            current_player = -current_player
+            
+            
+            
+            
+
+# %%
+def main():
+    game = TTT_gameplay()
+    
+    game.gameplay()
+    
+
+
+if __name__ == "__main__":
+    main()
+# %%
